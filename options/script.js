38527418f65e90d2e729fcadd7cd8202f5d6b56a
Trát Quang Thụy <babymouse011@gmail.com>
2022-12-24 02:48:56 +0700

    .

--- options/script.js
+++ options/script.js
@@ -4,28 +4,27 @@ const _defaultSettings = {
 };
 
 function isNumberKey(e) {
-  //showAlert('info', `${e.key} (${typeof e.key})`);
+  //showAlert(`${e.key} (${typeof e.key})`, 'info', false);
   if (!_num.has(e.key)) e.preventDefault();
 }
+
 /**
- *
- * @param {string} type Options ['', 'error', 'success', 'info', 'warning']
+ * Show message with Alert-Box.
  * @param {string} message
+ * @param {string} type - One of the options ['', 'error', 'success', 'info', 'warning']
  * @param {boolean} isAppend
  */
-function showAlert(type, message, isAppend) {
+function showAlert(message, type, isAppend) {
   const __alert_box = document.querySelector('.alert-box');
   __alert_box.className = 'alert-box';
   __alert_box.children[0].checked = false;
   const __alert = __alert_box.children[1];
   __alert.className = `alert ${type}`;
-  if (isAppend) {
-    //__alert.children[1].innerHTML += `<br \> ${message}`;
-    const __elem = document.createElement('p');
-    __elem.className = 'inner';
-    __elem.textContent = message;
-    __alert.append(__elem);
-  } else __alert.children[1].textContent = message;
+  const __inner = __alert.children[1];
+  const __elem = document.createElement('li');
+  __elem.textContent = message;
+  if (!isAppend) __inner.replaceChildren();
+  __inner.append(__elem);
 }
 
 function saveOptions() {
@@ -42,10 +41,10 @@ function saveOptions() {
     browser.storage.local.set({
       timeout: getTimeout(),
     });
-    showAlert('success', 'Successfully saved settings.', false);
+    showAlert('Successfully saved settings.', 'success', false);
   } catch (error) {
-    showAlert('error', error, false);
-    showAlert('error', 'Settings have not been saved.', true);
+    showAlert(error, 'error', false);
+    showAlert('Settings have not been saved.', 'error', true);
   }
 }
 
@@ -53,11 +52,11 @@ function updateUI(restoredSettings) {
   const __timeout = parseInt(restoredSettings.timeout);
   if (Number.isInteger(__timeout)) {
     if (__timeout < 0) {
-      showAlert('error', `[browser.storage.local.get()] Timeout must be >= 0 - Rertore default.`, false);
+      showAlert(`[browser.storage.local.get()] Timeout must be >= 0 - Rertore default.`, 'error', false);
       document.getElementById('timeout').value = _defaultSettings;
     } else document.getElementById('timeout').value = __timeout;
   } else {
-    showAlert('error', `[browser.storage.local.get()] Timeout must be number - Rertore default.`, false);
+    showAlert(`[browser.storage.local.get()] Timeout must be number - Rertore default.`, 'error', false);
     document.getElementById('timeout').value = _defaultSettings.timeout;
   }
 }
@@ -67,7 +66,7 @@ function restoreDefault() {
 }
 
 function onError(e) {
-  showAlert('error', `[browser.storage.local] ${e}`, false);
+  showAlert(`[browser.storage.local] ${e}`, 'error', false);
 }
 
 browser.storage.local.get().then(updateUI, onError);
